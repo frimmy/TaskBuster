d2245ca04795fcded9f1eba25b9daaa2b7eeb7fa
afrimpong <adrian.frimpong@gmail.com>
2015-01-13 12:31:25 -0500

    Updated templates, url routing for robots/humnans txt files

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'taskbuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name='home-files'), 
     url(r'^admin/', include(admin.site.urls)),
 )
